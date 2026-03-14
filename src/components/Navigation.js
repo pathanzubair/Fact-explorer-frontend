71ad7278995ad09eeb547d7065443079bb5a18f5
Zubair Pathan <pathanzubairaziz@gmail.com>
2026-03-14 12:08:43 +0530

    link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,10 +7,14 @@ const Navigation = () => {
       <div className="nav-container">
         <Link to="/" className="nav-logo">IPQuest</Link>
         <ul className="nav-menu">
-          <li><Link to="https://quizzy-frontend-c27i.onrender.com">Home</Link></li>
+          <li><Link to="/">Home</Link></li>
           
-          {/* 🟢 CORRECT: Only use the path name defined in your App.js Route */}
-          <li><Link to="/reference-hub">Reference Hub</Link></li>
+          {/* 🟢 Use <a> for a completely separate Render site */}
+          <li>
+            <a href="https://reference-be-1.onrender.com/" target="_blank" rel="noopener noreferrer">
+              Reference Hub
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
